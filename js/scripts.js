82c2fc496c1faa458870bbc049143d1f84c4bd18
Muhammad-Awab <muhammadawab82002@gmail.com>
2022-06-21 12:41:11 +0500

    scss apdated

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $(".begin").clic(function () {
+
+      $("#carouselExampleDark").carousel('cycle');
+    });
+    $(".pause").click(function () {
+
+      $("#carouselExampleDark").carousel('pause');
+    });
+  });
+
+
+  // $(function(){
+  //   $('#showBtn').click(function(){
+  //     $('#exampleModal').modal('show') ;
+  //   })
+  // });
+  const myModal = document.getElementById('exampleModal')
+  const myInput = document.getElementById('showBtn')
+
+  myInput.addEventListener('click', () => {
+    $('#exampleModal').modal('show')
+  })
+  const myModal1 = document.getElementById('exampleModal1')
+  const myInput1 = document.getElementById('showBtn1')
+
+  myInput1.addEventListener('click', () => {
+    $('#exampleModal1').modal('show')
+  })
+  
